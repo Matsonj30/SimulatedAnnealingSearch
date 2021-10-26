5f699567d800cd87fb2ff96b432e4f79c2a2c7a0
Jared Matson <40784129+Matsonj30@users.noreply.github.com>
2021-10-26 16:02:00 -0600

    print statements

--- lab4.py
+++ lab4.py
@@ -1,6 +1,6 @@
 import copy
-
-
+import random
+import math
 
 class Node:
     def __init__(self, value, parent, goodOrBad) -> None:
@@ -15,22 +15,55 @@ def simulatedAnnealing(current):
     t = 1
     alreadyTravelledStates = [current]
     potentialMoves = []
-    #while True:
-    t += 1
-    T = schedule(t)
-    if (T == 0):
-        return current
-    for action in possibleActions(current, alreadyTravelledStates):
-        potentialMoves.append(action)
-    
-    
+    print("Initial state:")
+    printboard(current.value)
+    print("")
+    while True:
+        t += 1
+        T = schedule(t)
+        if (T == 0):
+            return current
+        for action in possibleActions(current, alreadyTravelledStates):
+            potentialMoves.append(action)
+        next = potentialMoves[random.randint(0, len(potentialMoves) - 1)]
+        #higher manhattanValue = better
+        deltaE = manHattanValue(next) - manHattanValue(current)
+        if deltaE > 0:
+            alreadyTravelledStates.append(next)
+            current = next
+            printboard(current.value)
+            print("(value = ",end="")
+            print(current.h,end="")
+            print(")")
+            print("")
+        else:
+            print(math.exp(deltaE/T))
+            if random.uniform(0,1) < math.exp(deltaE/T): #math is backwards here , will always go here
+                alreadyTravelledStates.append(next)
+                current = next
+                printboard(current.value)
+                print("(value = ",end="")
+                print(current.h, end="")
+                print("), BAD MOVE was chosen")
+                print("")
+        potentialMoves = []
 
 def schedule(t):
-    alteredTValue = 1000000
+    alteredTValue = 5
     while t > 0:
-        alteredTValue = alteredTValue / 1.5
+        alteredTValue = alteredTValue - 0.01
         t = t - 1
     return alteredTValue
+
+def printboard(board):
+    for row in board:
+        for value in row:
+            if value == 0:
+                print("  ", end='')
+            else:
+                print(str(value) + " ", end='')
+        print("")
+        
 
 def manHattanValue(state):
     HValue = 0
@@ -69,6 +102,7 @@ def manHattanValue(state):
                 elif value[row][col] == 8:
                     HValue += abs(eightGoal[0] - row)
                     HValue += abs(eightGoal[1] - col)
+    state.h = 0 - HValue
     return(0-HValue)
 
 def possibleActions(currentState, alreadyTraveledStates):
@@ -118,6 +152,9 @@ def possibleActions(currentState, alreadyTraveledStates):
 
 
    # def Value
-x=Node([[8,1,2],[3,4,5],[6,7,0]], None, "N/A")
-#simulatedAnnealing(Node([[0,1,2],[3,4,5],[6,7,8]], None, "N/A"))
-print(manHattanValue(x))
+#x=Node([[8,1,2],[3,4,5],[6,7,0]], None, "N/A")
+simulatedAnnealing(Node([[1,2,3],[4,5,8],[6,7,0]], None, "N/A"))
+#print(manHattanValue(x))
+#print(schedule(100))
+#print(random.uniform(0,1))
+#print(math.exp(-5/5)) #200 seems highest we can go
